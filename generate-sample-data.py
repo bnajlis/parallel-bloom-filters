85711447b17ff0582a140ba0e10a50142140ce1d
Bernardo Najlis <b52.net@gmail.com>
2017-11-18 18:22:14 -0500

    Adds generate-sample-data program to create sample input test files

---
+++ generate-sample-data.py
@@ -0,0 +1,45 @@
+"""
+Random sample data generator
+============================
+Use to generate a file with random strings of fixed length.
+Usage: python3 generate-sample-data.py -samples [s] -length [l] -output [f]
+Example: python3 generate-sample-data.py -samples 1000 -length 10 -output sample_data.txt
+will generate 1000 strings with 10 random characters into sample_data.txt
+"""
+import sys
+import random
+import string
+import os
+
+def generate_samples(ns, sl, of):
+    """Generate file with random sample strings of fixed length"""
+    file = open(of, "w")
+    valid_chars = string.ascii_uppercase + string.digits
+    for x in range(0, ns):
+        sample = ''.join(random.choice(valid_chars) for _ in range(sl))
+        file.write(sample + os.linesep)
+    file.close()
+
+def getopts(argv):
+    """Get command line arguments"""
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
+
+if __name__ == '__main__':
+    myargs = getopts(sys.argv)
+    if '-samples' and '-length'  and '-output' in myargs:
+        num_samples = int(myargs['-samples'])
+        sample_length = int(myargs['-length'])
+        output_filename = myargs['-output']
+        generate_samples(num_samples, sample_length, output_filename)
+    else:
+        print("Random sample data generator")
+        print("============================")
+        print("Use to generate a file with random strings of fixed length.")
+        print("Usage: python3 generate-sample-data.py -samples [s] -length [l] -output [f]")
+        print("Example: python3 generate-sample-data.py -samples 1000 -length 10 -output sample_data.txt")
+        print("will generate 1000 strings with 10 random characters into sample_data.txt")
