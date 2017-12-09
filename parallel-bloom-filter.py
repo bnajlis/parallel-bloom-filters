258b115364b000ad2b81603de890447f2f6a8f67
Bernardo Najlis <b52.net@gmail.com>
2017-12-09 14:23:40 -0500

    Updated bloom filter program with code to create bloom filter

--- parallel-bloom-filter.py
+++ parallel-bloom-filter.py
@@ -17,30 +17,67 @@ def create_spark_context():
     sc = SparkContext("local", "ParallelBloomFilter")
     return sc
 
-def bloom_filter(in_fname, out_fname):
+def create_bloom_filter(in_fname, out_fname):
+    
     """Creates a Bloom Filter index from input filename and writes to out filename"""
     sc = create_spark_context()
-    in_data = sc.textFile(in_fname)
-    #print(int(hashlib.md5(b"Hello World!").hexdigest(),16))
 
-def hash_1(input):
-    return int(hashlib.md5(input.encode("utf-8")).hexdigest(),16) % (10 ** 5))
+    # Read the input file into an RDD
+    in_data = sc.textFile(in_fname)
+    # Split the input file into multiple lines
+    in_split = in_data.flatMap(lambda x: x.split())
+
+    # Apply four different hash functions to create the Bloom filter
+    # Each one of these RDDs has the indice that will be set to 1
+    # to mark the existance of the input elements
+    in_md5    = in_split.map(lambda x: int(hashlib.md5(x.encode("utf-8")).hexdigest(),16) % (10 ** 6))
+    in_sha224 = in_split.map(lambda x: int(hashlib.sha224(x.encode("utf-8")).hexdigest(),16) % (10 ** 6))
+    in_sha256 = in_split.map(lambda x: int(hashlib.sha256(x.encode("utf-8")).hexdigest(),16) % (10 ** 6))
+    in_sha384 = in_split.map(lambda x: int(hashlib.sha384(x.encode("utf-8")).hexdigest(),16) % (10 ** 6))
+
+    # Union all sets of indices, so we know the full collection of indices that have to be set to 1
+    in_allhashes = in_md5.union(in_sha384).union(in_sha256).union(in_sha224)
+
+    # As multiple hash functions can map the same input element to the same index,
+    # we need to remove duplicates
+
+    # Create a set per index, so we can use reduceByKey to remove duplicates
+    in_mapped = in_allhashes.map(lambda x: (x, 1))
+    # Use reduceByKey to remove index duplicates (d)
+    in_nodupes = in_mapped.reduceByKey(lambda x, y: 1)
+    # This is the list of indices set to 1 in the Bloom Filter
+    bloom_indices = in_nodupes.map(lambda x: x[0])
+    # Save Bloom Filter indices to output file
+    bloom_indices.saveAsTextFile(out_fname)
+
+def validate_bloom_filter(infile, item):
+    return 0
 
 
 if __name__ == '__main__':
     myargs = getopts(sys.argv)
     if '-create' and '-data' and '-index' in myargs:
-        input = myargs['-data']
-        output = myargs['-index']
-        bloom_filter(input, output)
+        infile = myargs['-data']
+        outfile = myargs['-index']
+        create_bloom_filter(infile, outfile)
     elif '-validate' and '-index' in myargs:
-        input = myargs['-index']
+        infile = myargs['-index']
+        item = myargs['-validate']
+        validate_bloom_filter(infile, item)
     else:
-        print("Random sample data generator")
+        print("Parallel Bloom Filter Generator")
         print("============================")
-        print("Use to generate a file with random strings of fixed length.")
-        print("Usage: python3 generate-sample-data.py -samples [s] -length [l] -output [f]")
-        print("Example: python3 generate-sample-data.py -samples 1000 -length 10 -output sample_data.txt")
-        print("will generate 1000 strings with 10 random characters into sample_data.txt")
+        print()
+        print("Use to generate a Bloom Filter indices file,")
+        print("and validate if an item exists on a previously created index.")
+        print()
+        print("Usage:")
+        print("To create a Bloom filter index file from an existing data file:")
+        print("   spark-submit parallel-bloom-filter.py -create -data [data_filename] -index [index_output_filename]")
+        print("Example: spark-submit parallel-bloom-filter.py -create -data ./data.dat -index ./bloom_filter.dat")
+        print()
+        print("To validate an item existing in a previously created filter:")
+        print("   spark-submit parallel-bloom-filter.py -validate -index [index_output_filename] -item [item_to_validate]")
+        print("Example: spark-submit parallel-bloom-filter.py -validate -index ./bloom_filter.dat -item QGTYPF&UOHY")
 
 
